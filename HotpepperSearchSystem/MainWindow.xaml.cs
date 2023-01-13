4515227fbd7afa95ee8f8a490303c0f736b9ec1b
33029二渡絢世 <33029@ojs.ac.jp>
2023-01-13 09:32:06 +0900

    卒業制作途中

--- HotpepperSearchSystem/MainWindow.xaml.cs
+++ HotpepperSearchSystem/MainWindow.xaml.cs
@@ -22,19 +22,49 @@ namespace HotpepperSearchSystem {
     public partial class MainWindow : Window {
 
         BitmapImage m_bitmap = null;
-
+        private NavigationService _navi;
         public MainWindow() {
             InitializeComponent();
+            _navi = this.myFrame.NavigationService;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) {
+        private List<Uri> _uriList = new List<Uri>() {
+            new Uri("Page1.xaml",UriKind.Relative)
+            //new Uri("Page2.xaml",UriKind.Relative)
+        };
+
+        private void myFrame_Loaded(object sender, RoutedEventArgs e) {
+            _navi.Navigate(_uriList[0]);    //初期ページの表示
+        }
+
+        private void myFrame_Navigated(object sender, NavigationEventArgs e) {
+            int index = _uriList.IndexOf(_navi.CurrentSource);
+            //if (index <= 0)
+            //    prevButton.IsEnabled = false;
+            //else
+            //    prevButton.IsEnabled = true;
+            if (index == _uriList.Count)
+                searchButton.IsEnabled = false;
+            else
+                searchButton.IsEnabled = true;
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e) {
+
+            if (_navi.CanGoForward)
+                _navi.GoForward();
+            else {
+                int index = _uriList.FindIndex(p => p == _navi.CurrentSource) + 1;
+                _navi.Navigate(_uriList[index]);    //ページの移動
+            }
+
             var wc = new WebClient() {
                 Encoding = Encoding.UTF8
             };
             //var dString = " ";
             //dString = wc.DownloadString("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=dce4542a04b9085c&large_area=Z017&format=json");
             //var json = JsonConvert.DeserializeObject<Results[]>(dString);
-            //SearchTB.Text = json[0].shop[0].name;
+            //SearchTB.Text = json[0].shop[0].access;
         }
 
         private void Grid_Loaded(object sender, RoutedEventArgs e) {
